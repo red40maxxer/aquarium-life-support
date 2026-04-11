2ff7d624d69145b24e16a2f0536d29edaf8e4361
mad-cat-lon <113548315+mad-cat-lon@users.noreply.github.com>
2026-04-11 18:01:43 -0400

    feat: display temp and timestamp

--- display.py
+++ display.py
@@ -5,6 +5,29 @@ import logging
 import spidev as SPI
 from lib import LCD_2inch
 from PIL import Image,ImageDraw,ImageFont
+import glob
+from datetime import datetime
+
+base_dir = '/sys/bus/w1/devices/'
+device_folder = glob.glob(base_dir + '28*')[0]
+device_file = device_folder + '/w1_slave'
+
+def read_temp_raw():
+    f = open(device_file, 'r')
+    lines = f.readlines()
+    f.close()
+    return lines
+ 
+def read_temp():
+    lines = read_temp_raw()
+    while lines[0].strip()[-3:] != 'YES':
+        time.sleep(0.2)
+        lines = read_temp_raw()
+    equals_pos = lines[1].find('t=')
+    if equals_pos != -1:
+        temp_string = lines[1][equals_pos+2:]
+        temp_c = float(temp_string) / 1000.0
+        return temp_c
 
 # Pin configs 
 RST = 27
@@ -22,14 +45,28 @@ try:
     #Set the backlight to 100
     disp.bl_DutyCycle(50)
 
-    image1 = Image.new("RGB", (disp.height, disp.width ), "BLACK")
-    draw = ImageDraw.Draw(image1)
+    font_temp = ImageFont.truetype("agamefont.ttf", 32)
+    font_small = ImageFont.truetype("agamefont.ttf", 16)
+    font_status = ImageFont.truetype("agamefont.ttf", 18)    
 
-    font = ImageFont.truetype("agamefont.ttf",25)
-    draw.text((5, 58), 'hello world', fill="WHITE", font=font)
-    
-    disp.ShowImage(image1)
-    
+    # keep it running forever
+    while True:
+        # refresh image each time 
+        image = Image.new("RGB", (disp.height, disp.width ), "BLACK")
+        draw = ImageDraw.Draw(image)
+
+        # header
+        curr_temp = read_temp()
+        curr_time_str = datetime.now().strftime("%d-%m-%Y %H:%M:%S")
+        draw.text((180, 10), f"{curr_time_str}", fill="WHITE", font=font_temp)
+        draw.text((8, 8), f"{curr_temp} C", fill="WHITE", font=font_small)
+        #TODO: implement status logic
+        draw.text((8, 44), "stablizing", fill="WHITE", font=font_small)
+
+        disp.ShowImage(image)
+        # TODO: figure out optimal polling period
+        time.sleep(1)
+
 except IOError as e:
     logging.info(e)    
 except KeyboardInterrupt:
